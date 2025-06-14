bbe1629ab81c9f8747775908840dd7413498b53f
MatheusAnimacaoCraft <matheuscustodio706@gmail.com>
2025-06-14 08:53:59 -0300

    atulização html css

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -4,12 +4,23 @@ searchInput.addEventListener('input', (event) => {
     const value = formatString(event.target.value);
 
     const items = document.querySelectorAll('.items .item');
+    const noResults = document.getElementById('no_results');
+
+    let hasResults = false;
 
     items.forEach(item => {
         if(formatString(item.textContent).indexOf(value) !== -1) {
             item.style.display = 'flex'
+
+            hasResults = true;
         }else {
-            item.style.display = 'none'
+            item.style.display = 'none';
+        }
+
+        if(hasResults) {
+            noResults.style.display = 'none';
+        }else {
+            noResults.style.display = 'block';
         }
     })
 });
